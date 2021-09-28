6aecf406324e5b37258b44071cbaf0f281a3a504
unknown <johan7laursen@gmail.com>
2021-09-28 12:47:51 +0200

    add CityRecord

---
+++ Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityRecord.java
@@ -0,0 +1,12 @@
+package edu.sdse.csvprocessor;
+
+public class CityRecord {
+	int id, year, population;
+	String city;
+	
+	CityRecord(int id, int year, int population, String city){
+        this.id = id;
+        this.year = year;
+        this.population = population;
+        this.city = city;
+}
